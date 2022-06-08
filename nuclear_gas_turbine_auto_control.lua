c4b904a4b5889334525977e884eec1d888adc662
FinnianLR <finnniansproject@gmail.com>
2022-06-08 21:46:23 +0200

    Added gas turbine control

--- nuclear_gas_turbine_auto_control.lua
+++ nuclear_gas_turbine_auto_control.lua
@@ -9,6 +9,7 @@ local computer = require("computer")
 local term = require("term")
 
 reactor = component.nc_fission_reactor
+turbine = component.it_gas_turbine
 
 print("-----RESETTING BACKUP-----")
 rs.setBundledOutput(sides.left, colors.green, 0)
@@ -22,12 +23,26 @@ function totalPowerLevel()
   return reactor.getEnergyStored()
 end
 
+function checkPowerSwitch()
+  if rs.getBundledInput(sides.left, colors.orange) > 0 then
+    rs.setBundledOutput(sides.left, colors.red, 255)
+    print("Reactor status: ON")
+  elseif rs.getBundledInput(sides.left, colors.orange) == 0 then
+    rs.setBundledOutput(sides.left, colors.red, 0)
+    print("Reactor status: OFF")
+  end
+end
+
+
+
 backup = false
 
 function startGasTurbine()
   print("=====Starting Gas Turbine=====")
+  print("Disconnecting from grid")
   computer.beep(500, 1)
   computer.beep(700, 1)
+  rs.setBundledOutput(sides.left, colors.magenta, 255)
   print("Generator start: status:ON, spool:ON, ignition:ON")
   rs.setBundledOutput(sides.left, colors.black, 255)
   rs.setBundledOutput(sides.left, colors.yellow, 255)
@@ -41,7 +56,33 @@ function startGasTurbine()
   -- Turn off dieselGen
   print("Generator start: Diesel-Backup:OFF")
   rs.setBundledOutput(sides.left, colors.white, 0)
+  print("Connecting back to grid")
+  rs.setBundledOutput(sides.left, colors.magenta, 0)
+  os.sleep(5)
   backup = true
+  
+  while(backup)
+  do
+    os.sleep(1)
+    term.clear()
+   
+     -- Print info about current state of backup  system.
+
+    print("Current turbine speed: ", turbine.getSpeed())
+    print("Diesel backup level: ", ((rs.getBundledInput(sides.left, colors.cyan)/195)*100))
+
+    if totalPowerLevel() > 1000 then
+      backup = false
+    end
+
+    if rs.getInput(sides.back) > 0 then
+      -- shutdown system
+      x = false
+      backup = false
+    end
+    checkPowerSwitch()
+
+  end
 end
 
 function resetBackUp()
@@ -66,11 +107,23 @@ do
     os.sleep(1)
     term.clear()
     print("Power in reactor: ", reactor.getEnergyStored())
+    print("Reactor temprature:  ", reactor.getHeatLevel())
+    print("Fuel heat: ", reactor.getFissionFuelHeat())
+    print("Number of cells in use", reactor.getNumberOfCells())
+    print("with: ", reactor.getFissionFuelName())
+    print("Current cooling rate: ", reactor.getReactorCoolingRate())
+    print("Max heat level is: ", reactor.getMaxHeatLevel())
+    print("Current process time: ", reactor.getCurrentProcessTime())
     print("Power in base: ", totalPowerLevel())
+    print("Gas Turbine Status", turbine.getSpeed())
+    print("Diesel backup level: ", ((rs.getBundledInput(sides.left, colors.cyan)/195)*100))
+    checkPowerSwitch()  
   end
-
+  
+  -- Check if program termination switch is triggered.
   if rs.getInput(sides.back) > 0 then
     resetBackUp()
     x = false
-  end 
+  end
+
 end
